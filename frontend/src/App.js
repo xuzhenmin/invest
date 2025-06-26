c7cbda90545f3e09a21826a1cb64226c620edde8
laoxu <823888251@qq.com>
2025-06-26 13:52:51 +0800

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Dashboard from './pages/Dashboard';
+import 'antd/dist/reset.css';
+
+function App() {
+  console.log('App component rendered.');
+  return <Dashboard />;
+}
+
+export default App; 
