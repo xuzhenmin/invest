9e320226aeac8cfbd6b4da211229925a871e9628
laoxu <823888251@qq.com>
2025-07-22 19:09:19 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Dashboard from './pages/Dashboard';
+import Watchlist from './pages/Watchlist';
 import 'antd/dist/reset.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  console.log('App component rendered.');
-  return <Dashboard />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/watchlist" element={<Watchlist />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App; 
